dbb1f9898bdffbef427e524eec3b557a2c45c83f
Optidox <32784225+Optidox@users.noreply.github.com>
2020-05-04 19:33:16 -0500

    Initial commit to GitHub

---
+++ app/routes.py
@@ -0,0 +1,36 @@
+from flask import render_template, flash, redirect, url_for, request
+from app import app
+from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+from werkzeug.urls import url_parse
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home', user=None)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
