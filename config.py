eec11486014ae5b13836728ddaa1b4e83e56264e
Matthew Sims <optidox@gmail.com>
2020-12-08 00:30:17 +0000

    Cookie security updated to current standard

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
