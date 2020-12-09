6552b8b133563be69df18e74eac15961a786c0f7
Optidox <32784225+Optidox@users.noreply.github.com>
2020-12-09 00:56:19 -0600

    Moved js to own file

---
+++ app/static/navbar.js
@@ -0,0 +1,21 @@
+// Change style of navbar on scroll
+console.log("fuckfuckfuckfukfrufuf");
+
+document.addEventListener("scroll", () => {
+    let navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+});
+
+/*// Used to toggle the menu on small screens when clicking on the menu button
+document.getElementById("bars").addEventListener("click", () => {
+    let x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+});*/
